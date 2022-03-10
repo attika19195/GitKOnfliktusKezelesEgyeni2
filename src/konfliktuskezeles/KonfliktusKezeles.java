6923acbf60145e7be75a48cf32981d12a43c84c6
attika19195 <attika19195@gmail.com>
2022-03-10 12:40:24 +0100

    3.név deklarálása

--- src/konfliktuskezeles/KonfliktusKezeles.java
+++ src/konfliktuskezeles/KonfliktusKezeles.java
@@ -4,8 +4,10 @@ package konfliktuskezeles;
 public class KonfliktusKezeles {
 
     public static void main(String[] args) {
+        int i = 7;
         String nev1 = "PA";
         String nev2 = "X";
+        int nev3;
     }
     
 }
